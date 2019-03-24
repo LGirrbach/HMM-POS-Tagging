efae70ddd35f87b85db6007af13587e2f26987b7
girrbach <girrbach@cl.uni-heidelberg.de>
2019-03-24 15:33:38 +0100

    Use Gutenberg as training data for Baum-Welch

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pickle
 
 from nltk.corpus import treebank as training_corpus
 from nltk.corpus import conll2000 as test_corpus
-from nltk.corpus import inaugural as learn_corpus
+from nltk.corpus import gutenberg as learn_corpus
 from nltk.data import load
 
 import numpy as np
@@ -25,11 +25,14 @@ if __name__ == '__main__':
     UNK = "UNK"
     ALL_TAGS = tuple(sorted(load('help/tagsets/upenn_tagset.pickle').keys()))
     create_new_model = True
+    
+    learn_corpus_sentences = [[word.lower() for word in sentence]
+                              for sentence in learn_corpus.sents()]
 
     logging.info("Learn Model")
     if create_new_model:
         learner = ForwardBackwardAlgorithm(ALL_TAGS, np.random.dirichlet(np.ones(len(ALL_TAGS)), 1)[0], START, STOP)
-        my_hmm = learner.learn_hmm(list(learn_corpus.sents()))
+        my_hmm = learner.learn_hmm(learn_corpus_sentences)
         with open("HMM_model.dat", "wb") as hmm_file:
             pickle.dump(my_hmm, hmm_file)
     else:
